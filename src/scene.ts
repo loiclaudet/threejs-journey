51ab031fe15fc6b49835952de50fb81ad51b4a87
loiclaudet <8344874+loiclaudet@users.noreply.github.com>
2023-11-02 15:31:04 +0100

    feat: add shadows using several techniques

--- src/scene.ts
+++ src/scene.ts
@@ -20,22 +20,78 @@ export const setupScene = (canvasElement: HTMLCanvasElement) => {
     y: 0,
   }
 
+  const textureLoader = new THREE.TextureLoader()
+  // const bakedShadow = textureLoader.load("/textures/bakedShadow.jpg")
+  const simpleShadow = textureLoader.load("/textures/simpleShadow.jpg")
+
   /**
    * Lights
    */
   // Ambient light
-  const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
   gui.add(ambientLight, "intensity").min(0).max(2).step(0.001)
   scene.add(ambientLight)
 
   // Directional light
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
   directionalLight.position.set(2, 2, -1)
-  gui.add(directionalLight, "intensity").min(0).max(1).step(0.001)
+  directionalLight.castShadow = false
+  directionalLight.shadow.mapSize.width = 1024
+  directionalLight.shadow.mapSize.height = 1024
+
+  // shadow camera for directional light is an orthographic camera
+  directionalLight.shadow.camera.near = 1
+  directionalLight.shadow.camera.far = 6
+  // reducing the amplitude provide a better shadow result
+  directionalLight.shadow.camera.top = 2
+  directionalLight.shadow.camera.right = 2
+  directionalLight.shadow.camera.bottom = -2
+  directionalLight.shadow.camera.left = -2
+  // directionalLight.shadow.radius = 10
+  scene.add(directionalLight)
+
+  const directionalLightCameraHelper = new THREE.CameraHelper(
+    directionalLight.shadow.camera
+  )
+  directionalLightCameraHelper.visible = false
+  scene.add(directionalLightCameraHelper)
+
+  gui.add(directionalLight, "intensity").min(0).max(4).step(0.001)
   gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001)
   gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001)
   gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001)
-  scene.add(directionalLight)
+
+  // Spot light
+  const spotLight = new THREE.SpotLight(0xffffff, 2, 10, Math.PI / 3)
+  spotLight.castShadow = false
+  spotLight.position.set(0, 2, 2)
+  scene.add(spotLight, spotLight.target)
+  spotLight.shadow.mapSize.width = 1024
+  spotLight.shadow.mapSize.height = 1024
+  // shadow camera for directional light is a perspective camera
+  spotLight.shadow.camera.fov = 30
+  spotLight.shadow.camera.near = 1
+  spotLight.shadow.camera.far = 6
+
+  const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+  spotLightCameraHelper.visible = false
+  scene.add(spotLightCameraHelper)
+
+  // Point light
+  const pointLight = new THREE.PointLight(0xffffff, 1)
+  pointLight.position.set(-1, 1, 0)
+  pointLight.castShadow = false
+  pointLight.shadow.mapSize.width = 1024
+  pointLight.shadow.mapSize.height = 1024
+  pointLight.shadow.camera.near = 0.1
+  pointLight.shadow.camera.far = 5
+  scene.add(pointLight)
+
+  const pointLightCameraHelper = new THREE.CameraHelper(
+    pointLight.shadow.camera
+  )
+  pointLightCameraHelper.visible = false
+  scene.add(pointLightCameraHelper)
 
   /**
    * Materials
@@ -49,12 +105,31 @@ export const setupScene = (canvasElement: HTMLCanvasElement) => {
    * Objects
    */
   const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
+  sphere.castShadow = false
 
-  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
+  const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    new THREE.MeshBasicMaterial({
+      // map: bakedShadow,
+    })
+  )
+  plane.receiveShadow = true
   plane.rotation.x = -Math.PI * 0.5
   plane.position.y = -0.5
 
   scene.add(sphere, plane)
+
+  const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      alphaMap: simpleShadow,
+      transparent: true,
+    })
+  )
+  sphereShadow.rotation.x = -Math.PI / 2
+  sphereShadow.position.y = plane.position.y + 0.01
+  scene.add(sphereShadow)
 
   window.addEventListener("mousemove", (event: MouseEvent) => {
     cursor.x = event.clientX / sizes.width - 0.5
@@ -102,14 +177,26 @@ export const setupScene = (canvasElement: HTMLCanvasElement) => {
   const renderer = new THREE.WebGLRenderer({
     canvas: canvasElement,
   })
-
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
   renderer.outputColorSpace = THREE.LinearSRGBColorSpace
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
   renderer.render(scene, camera)
 
+  const clock = new THREE.Clock()
+
   const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = 1 - sphere.position.y * 0.5
+
     controls.update()
 
     renderer.render(scene, camera)
